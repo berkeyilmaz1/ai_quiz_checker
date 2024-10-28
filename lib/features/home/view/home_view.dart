cd14a6b9c9cc9ec06653dfaaf2d79afc18f0c2c7
berke.yz <berkeyilmaz189@gmail.com>
2024-10-28 15:57:55 +0300

    folders created

---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:ai_quiz_checker/product/widget/custom_elevated_button.dart';
+import 'package:flutter/material.dart';
+
+final class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const CustomElevatedButton();
+  }
+}
