bb1b1f3bcd80657eefeab26c63a1c6725343c59d
Berke Yılmaz <115240560+berkeyilmaz1@users.noreply.github.com>
2024-11-11 21:00:44 +0300

    Update product_constants.dart

--- lib/product/utils/constants/product_constants.dart
+++ lib/product/utils/constants/product_constants.dart
@@ -1,26 +1,3 @@
-// final class ProductConstants {
-//   static const appName = 'AI Quiz Checker';
-//   static const questionTitle = 'Questions';
-//   static const answerTitle = 'Answers';
-//   static const addAnswer = 'Add Answer';
-//   static const addQuestion = 'Add Question';
-//   static const edit = 'Edit';
-//   static const save = 'Save';
-//   static const areYouSure = 'Are you sure?';
-//   static const areYouSureQuestionContent =
-//       'Are you sure you want to save this question?';
-//   static const areYouSureAnswerContent =
-//       'Are you sure you want to save this answer?';
-//   static const no = 'No';
-//   static const yes = 'Yes';
-//   static const camera = 'Camera';
-//   static const gallery = 'Gallery';
-//   static const solve = 'Solve';
-//   static const resources = 'Resources';
-//   static const close = 'Close';
-//   static const bestAnswer = 'Best Answer';
-// }
-
 final class ProductConstants {
   static const appName = 'AI Quiz Checker';
   static const questionTitle = 'Sorular';
